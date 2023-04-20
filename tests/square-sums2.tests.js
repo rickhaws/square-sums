57367b24990ffa0ea26ec84afed0377393657549
Rick Haws <61323632+rickhaws@users.noreply.github.com>
2023-04-20 11:22:53 -0600

    Update square-sums2.tests.js

--- tests/square-sums2.tests.js
+++ tests/square-sums2.tests.js
@@ -1,84 +1,83 @@
-square_sums_row = square_sums_row || squareSumsRow
+function removeUsedNodesFromMap(map, sequence) {
+  sequence = Array.isArray(sequence) ? sequence : [];
+  map = new Map(map);
+  for (const used of sequence) {
+    const neighbors = map.get(used);
+    if (neighbors === undefined) { continue; }
+    for (const neighbor of neighbors) {
+      const neighborPairs = map.get(neighbor);
+      if (neighborPairs === undefined) { continue; }
+      map.set(neighbor, neighborPairs.filter(n => n !== used));
+    }
+    map.delete(used);
+  }
+  return map;
+}
 
-describe('Verify sequences', function() {
-  function verify(n, r) {
-    if (!Array.isArray(r)) {
-      Test.expect(false, 'Not an array');
-      return
-    }
-    let act_sorted = JSON.stringify(r.slice(0).sort((a, b)=>a-b))
-    let exp_sorted = JSON.stringify(Array(n).fill(0).map((a,i)=>i+1))
-    Test.assertEquals(act_sorted, exp_sorted, 'Some numbers are missing')
-    if (act_sorted !== exp_sorted) {
-      return;
-    }
-    for (let i = 1; i < n; ++i) {
-      let sum = r[i - 1] + r[i]
-      let not_sq = 0 !== Math.sqrt(sum) % 1
-      if (not_sq) {
-        Test.expect(false, `Pair sum is not a square: ${r[i - 1]}+${r[i]}=${sum}`)
-      }
-    }
+function sieve(pairs, sequence) {
+  let candidates;
+  if (!sequence || sequence.length === 0) {
+    sequence = [];
+    candidates = [...pairs.keys()];
+  }
+  else {
+    const last = sequence.slice(-1)[0];
+    candidates = pairs.get(last).filter(c => c !== last);
+    pairs = removeUsedNodesFromMap(pairs, sequence);
   }
   
-  function check(n) {
-    return verify(n, square_sums_row(n))
+  // Base case -- all numbers are in the sequence
+  if (pairs.size === 0) {
+//       /* */ console.log("****  ", JSON.stringify(sequence))
+    return [...sequence];
   }
   
-  it('Tests removing used nodes', function() {
-    let pairs = new Map([
-      [1, [2,5]],
-      [2, [1,3]],
-      [3, [2,4]],
-      [4, [3,5]],
-      [5, [4,1]]
-    ]);
-    let sequence = [1,2,3];
-    let filtered = removeUsedNodesFromMap(pairs, sequence);
-    sequence.forEach(used => {
-      Test.assertEquals(filtered.get(used), undefined);
-      for (let [k, v] of filtered) {
-//         console.log(k, v, used)
-        Test.assertNotContains(v, used);
-      }
-    });
-  });
+  //Optimizations
+  // Don't consider candidates without additional neighbors unless this is the last sequence item
+  if (pairs.size !== 1) { // Not the last candidate
+    candidates = [...candidates].filter(c => pairs.get(c).length !== 0)
+    
+    // Sort to test nodes with fewest neighbors first
+    candidates.sort((a,b) => pairs.get(a).length - pairs.get(b).length);
+  }
   
-  it('Tests sieve', function() {
-    let pairs = new Map([
-      [1, [2,5]],
-      [2, [1,3]],
-      [3, [2,4]],
-      [4, [3,5]],
-      [5, [4,1]]
-    ]);
-    Test.assertEquals(JSON.stringify(sieve(pairs)), JSON.stringify([1,2,3,4,5]));
-  });
- 
-  it('Basic test', function() {
-    check(15)
-    check(23)
-  })
+  // Sort to test nodes with fewest neighbors first
+  candidates.sort((a,b) => pairs.get(a).length - pairs.get(b).length);
+  // If more than one candidate has no neighbors, the sequence is impossible
+  if (candidates.length > 1 && pairs.get(candidates[1]).length === 0) {
+    return false;
+  }
   
-  it('No solution', function() {
-    for (let n of [2,14]) {
-      Test.assertEquals(square_sums_row(n), false)
+  // Find solution
+  let solution = false;
+  for (let p of candidates) {
+    sequence.push(p);
+    let newSequence = sieve(pairs, sequence);
+    if (newSequence) {
+      solution = newSequence;
+     break; // comment this line to find all solutions
     }
-  });
-  
-  it('Small numbers', function() {
-    for (let n = 25; n <= 45; ++n) {
-      check(n)
-    }
-  });
 
- // for extra challenge uncomment this (780 is NOT tested in final tests)
-  it('780', function() {
-    check(780)
-  })
+    sequence.pop();
+  }
   
- // My test to see how big it can go
-  it('BIG', function() {
-    check(6450);
-  })
-})
+  return solution;
+}
+
+function square_sums_row(n) {
+  const allSquares = [];
+  for (let i = 2 ; i*i < 2*n; i++) {
+    allSquares.push(i*i);
+  }
+  const pairs = new Map();
+  for (let i = 1; i<=n; i++) {
+    let pairsToI = allSquares.filter(q => 
+      q <= i+n && // Max sum is this number plus n
+      q > i && // Min square is this number plus 1
+      q !== 2 * i) // Can't be i + i: each number used exactly once
+    .map(qI => qI - i);
+    pairs.set(i, pairsToI);
+  }
+  
+  return sieve(pairs)
+}
